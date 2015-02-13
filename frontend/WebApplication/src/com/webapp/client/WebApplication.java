808783f9552fbda8a8f7af20bebf711519c937ff
mikebrevard <mikebrevard@gmail.com>
2015-02-12 18:38:39 -0800

    Changed web application to have read parameters from URL

--- frontend/WebApplication/src/com/webapp/client/WebApplication.java
+++ frontend/WebApplication/src/com/webapp/client/WebApplication.java
@@ -1,7 +1,9 @@
 package com.webapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 import com.webapp.client.userinterface.introduction.IntroductionPanel;
+import com.webapp.client.userinterface.mainpage.MainPanel;
 import com.webapp.client.userinterface.utilities.Toolkit;
 
 /**
@@ -13,6 +15,17 @@ public class WebApplication implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		Toolkit.loadWidget(new IntroductionPanel());
+
+		// example
+		// 127.0.0.1:8888/WebApplication.html?gwt.codesvr=127.0.0.1:9997&read=6&write=4
+		String read = Window.Location.getParameter("read");
+		String write = Window.Location.getParameter("write");
+		String file = Window.Location.getParameter("file");
+		String auto = Window.Location.getParameter("auto");
+
+		if (auto != null && auto.equals("off"))
+			Toolkit.loadWidget(new IntroductionPanel(read, write, file));
+		else
+			Toolkit.loadWidget(new MainPanel(read, write, file));
 	}
 }
--- frontend/WebApplication/src/com/webapp/client/userinterface/mainpage/MainPanel.java
+++ frontend/WebApplication/src/com/webapp/client/userinterface/mainpage/MainPanel.java
@@ -1,5 +1,7 @@
 package com.webapp.client.userinterface.mainpage;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -15,12 +17,32 @@ public class MainPanel extends ResourceWidget {
 	interface MainPanelUiBinder extends UiBinder<Widget, MainPanel> {
 	}
 
-	public MainPanel(String server, String login) {
+	public MainPanel(String read, String write, String file) {
 		initWidget(uiBinder.createAndBindUi(this));
 
-		header.setText("Connected to " + login + "@" + server);
+		String status = "Pass";
+		String message = "This is a generic message";
+
+		header.setText("Status: " + status);
+
+		Date date = new Date();
+		String text = date + ": Status is " + status
+				+ ". MySQL (read, write) is (" + read + ", " + write
+				+ "). Message is '" + message + "'";
+		results.setText(text);
+		pathInformation.setText("Information appended to '" + file + "'");
+
+		saveStats();
+	}
+
+	private void saveStats() {
+		// TODO:
 	}
 
 	@UiField
 	Label header;
+	@UiField
+	Label results;
+	@UiField
+	Label pathInformation;
 }
