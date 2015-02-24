d7d90e75447fa56338874ec02b5ae52a911aefcb
Zachary Sells <zacharysells@gmail.com>
2015-02-23 20:16:31 -0800

    Install tomcat on startup on webapp VM

--- vagrant/webbootstrap.sh
+++ vagrant/webbootstrap.sh
@@ -33,3 +33,27 @@ else
   echo "$WEBAPP not found" >> $VAGRANTLOG
   >&2 echo "$WEBAPP not found"
 fi
+
+
+# Install java
+cd /tmp/
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u75-b13/jdk-7u75-linux-i586.tar.gz"
+tar -xvf jdk-7u75-linux-i586.tar.gz
+sudo mkdir /usr/java/
+sudo mv jdk1.7.0_75 /usr/java/
+
+sudo echo "export JAVA_HOME=/usr/java/jdk1.7.0_75/" > /usr/local/tomcat7/bin/setenv.sh
+sudo chmod +x /usr/local/tomcat7/bin/setenv.sh
+
+# Install tomcat7
+cd /tmp/
+wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz
+tar -xvf apache-tomcat-7.0.59.tar.gz
+sudo mkdir /usr/local/tomcat7
+sudo mv /tmp/apache-tomcat-7.0.59/* /usr/local/tomcat7/
+
+# IDK
+yes | sudo yum install glibc.i686
+
+# Start tomcat
+sudo /usr/local/tomcat7/bin/startup.sh
