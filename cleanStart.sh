3a81bd5bf8f107a6356c4a3793630cec73b472b5
mikebrevard <mikebrevard@gmail.com>
2015-03-04 15:56:33 -0800

    fixed clean start

--- cleanStart.sh
+++ cleanStart.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+echo "Now deploy the newest web app"
+./deploy.sh
+
 echo "Start with a solid upcon to start your day.."
 cd vagrant/
 ./upcon.sh
 
-echo "Now deploy the newest web app"
-cd ../
-./deploy.sh
-
-echo "Now finally provision the world"
-cd vagrant/
-./provisioncon.sh
