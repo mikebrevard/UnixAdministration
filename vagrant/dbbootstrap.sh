7ecb9aad8629124b24c01febd236aed35b3a86d3
Zachary Sells <zacharysells@gmail.com>
2015-03-06 22:49:02 -0800

    db VM provision now done smarter

--- vagrant/dbbootstrap.sh
+++ vagrant/dbbootstrap.sh
@@ -2,39 +2,33 @@
 
 # instal mysql
 echo "installing mysql-server"
-if yum list installed mysql-server > /dev/null 2&>1; then
+if yum list installed mysql-server > /dev/null 2>&1; then
   echo "mysql-server already installed"
 else
-  sudo yum -y install mysql-server > /dev/null 2&>1;
-  /etc/init.d/mysqld restart > /dev/null 2&>1
+  sudo yum -y install mysql-server > /dev/null 2>&1;
+  /etc/init.d/mysqld restart > /dev/null 2>&1
   chkconfig mysqld on
   chkconfig --list mysqld
 fi
 
 # set up sql user account
 echo "creating user..."
-if [ ! -f /var/._dbinit183 ]; then
-  sudo mysql --user=root mysql < /vagrant/etc/oldSQL/createUser.sql
-  echo "User 'unixadmin' has been created with password 'unixadmin'"
-  sudo touch /var/._dbinit183
+if sudo mysql --user=root mysql < /vagrant/etc/oldSQL/createUser.sql > /dev/null 2>&1; then
+  echo "mysql user 'unixadmin' has been created with password 'unixadmin'"
 else
   echo "mysql user 'unixadmin' already created with password 'unixadmin'"
 fi
 
 # set up database and tables
 echo "initialize database and tables"
-if [ ! -f /var/._dbpop183 ]; then
-  sudo mysql < /vagrant/etc/data/createTable.sql
+if sudo mysql < /vagrant/etc/data/createTable.sql > /dev/null 2>&1; then
   sudo chmod 700 /vagrant/etc/data/importDataMySQL.sh
   /vagrant/etc/data/importDataMySql.sh
   echo "Populate completed."
-  sudo touch /var/._dbpop183
 else
   echo "database already populated"
 fi
 
-echo "deleting all the time"
-sudo rm -f /etc/localtime
 echo "linking real time"
 sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
